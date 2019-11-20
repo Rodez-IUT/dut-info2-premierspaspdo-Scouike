a5b9faf635fe42bd7d2ed2e50dfadac09f142ec2
Scouike <tatiana.borgi@iut-rodez.fr>
2019-11-20 09:50:04 +0100

    fix #3.1 Filtre sur status et username

--- all_users.php
+++ all_users.php
@@ -1,28 +1,78 @@
 <!DOCTYPE html>
-<html lang="fr">
-  <head>
-		<meta charset="utf-8">		
-   </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>All users</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
 
-  <body>
-		<?php
-			$host = 'localhost';
-			$db   = 'my-activities';
-			$user = 'root';
-			$pass = 'root';
-			$charset = 'utf8mb4';
-			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-			$options = [
-				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES   => false,
-			];
-			try {
-				$pdo = new PDO($dsn, $user, $pass, $options);
-			} catch (PDOException $e) {
-				throw new PDOException($e->getMessage(), (int)$e->getCode());
-			}
-			
-			$stmt = $pdo->query('SELECT * FROM users ORDER BY nom ');
-			
-		?>
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+    </style>
+</head>
+<body>
+
+
+<?php
+$host = 'localhost';
+$db = 'my_activities';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    echo $e->getMessage() ;
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+?>
+
+<h1>All Users</h1>
+
+<?php
+$start_letter = 'e';
+$status_id = 2 ;
+$sql = "select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id where username like '$start_letter%' and status_id = $status_id order by username";
+$stmt = $pdo->query($sql);
+?>
+
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Status</th>
+    </tr>
+
+    <?php while ($row = $stmt->fetch()) { ?>
+
+    <tr>
+        <td><?php echo $row['user_id']?></td>
+        <td><?php echo $row['username']?></td>
+        <td><?php echo $row['email']?></td>
+        <td><?php echo $row['status']?></td>
+    </tr>
+
+    <?php } ?>
+
+</table>
+
+</body>
+
+</html>
